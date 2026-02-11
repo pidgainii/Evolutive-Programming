652453dff397d4625fda3973c73368fb8f30c463
Yevgen <yevgenpetronyuk@gmail.com>
2026-02-11 15:37:29 +0100

    Creating population initialization with random chromosomes

--- Practica_1/src/main/java/practica/pe/Chromosome.java
+++ Practica_1/src/main/java/practica/pe/Chromosome.java
@@ -1,4 +1,28 @@
 package practica.pe;
 
+import java.util.ArrayList;
+
+
 public class Chromosome {
+
+    private String genotype;
+    private int xCoordinateSize;
+    private int yCoordinateSize;
+
+    public Chromosome(int boardSizeX, int boardSizeY, int nCameras) {
+        // Creating a random chromosome
+        int nBitsX = (int)(Math.log(boardSizeX) / Math.log(2)) + 1;
+        int nBitsY = (int)(Math.log(boardSizeY) / Math.log(2)) + 1;
+
+        int genotypeSize = (nBitsX + nBitsY) * nCameras;
+
+        // Inicializando de forma random el genotipo (las posiciones de las camaras)
+        this.genotype = java.util.stream.IntStream.range(0, genotypeSize)
+                .mapToObj(i -> String.valueOf(new java.util.Random().nextInt(2)))
+                .collect(java.util.stream.Collectors.joining());
+    }
+
+    public String getGenotype() {
+        return this.genotype;
+    }
 }
--- Practica_1/src/main/java/practica/pe/Main.java
+++ Practica_1/src/main/java/practica/pe/Main.java
@@ -1,13 +1,31 @@
 package practica.pe;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        final int populationSize = 10;
+        final int boardSizeX = 10;
+        final int boardSizeY = 10;
+        final int nCameras = 2;
+
+        // Creating population, which is an ArrayList of chromosomes
+        ArrayList<Chromosome> population = new ArrayList<Chromosome>(populationSize);
+
+        // Initializing population with random chromosomes
+        for (int i = 0; i < populationSize; i++)
+        {
+            population.add(new Chromosome(boardSizeX, boardSizeY, nCameras));
         }
+
+        for (Chromosome c: population)
+        {
+            System.out.println(c.getGenotype());
+        }
+
     }
 }
